fe9b18eafd788e46622bdbc685ba73d11830acd5
Ahsa21 <ahmadsalouka@gmail.com>
2024-11-17 04:50:05 -0800

    Initial Commit

---
+++ uppgift1/uppgift1App.swift
@@ -0,0 +1,17 @@
+//
+//  uppgift1App.swift
+//  uppgift1
+//
+//  Created by Ahmad Saloukha on 2024-11-17.
+//
+
+import SwiftUI
+
+@main
+struct uppgift1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
